fa18cfddbeecb50cca5d00413ccc9223985c67fa
Favas abdul nassar <favasabdulnassar@gmail.com>
2024-02-28 14:28:33 +0530

    Did binary search and linear search

---
+++ binarysearch.py
@@ -0,0 +1,31 @@
+# def linearsearch(target,array):
+#     for item in range(len(array)):
+#         if array[item] == target:
+#             return f"target element found at index {item}"
+#     return f"target element is not found"
+
+# array = [3,4,1,7,996,5]
+
+# print(linearsearch(11,array))
+
+
+def binarysearch(target,array):
+    low,high = 0 , len(array)
+
+    while low < high:
+        mid = (low + high)//2
+        mid_element = array[mid]
+        print(mid_element)
+
+        if mid_element == target:
+            return f"target found at index {mid}"
+        elif mid_element < target:
+             low = mid 
+        else:
+            high = mid 
+    return f"target element is not in this array"   
+
+
+
+array = [1,3,4,5,9,79]
+print(binarysearch(4,array))
