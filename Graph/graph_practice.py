51afec415d71c9a2b61614e97eb72c93e0d00658
Favas abdul nassar <favasabdulnassar@gmail.com>
2024-03-25 12:52:56 +0530

    Added Trie

--- Graph/graph_practice.py
+++ Graph/graph_practice.py
@@ -1,27 +1,27 @@
 class Graph:
     def __init__(self) -> None:
         self.graph = {}
-
+    
     def insert_vertex(self,vertex):
         if vertex not in self.graph:
             self.graph[vertex] = []
 
-
-    def add_edges(self,vertex,edge,bi=False):
+    def add_edges(self,vertex,edges,bi=False):
         self.insert_vertex(vertex)
-        self.insert_vertex(edge)
-        self.graph[vertex].append(edge) 
+        self.insert_vertex(edges)
+        self.graph[vertex].append(edges) 
+
         if bi:
-            self.graph[edge].append(vertex)
+            self.graph[edges].append(vertex)  
 
     def delete_vertex(self,vertex):
-        for vtx,edges in self.graph.items():
+        for vrtx,edges in self.graph.items():
             if vertex in edges:
-                self.graph[vtx].remove(vertex)
+                self.graph[vrtx].removes(vertex)
 
-        del self.graph[vertex]     
-
-    def Bfs(self):
+        del self.graph[vertex]                 
+    
+    def Bfs (self):
         traversal = []
 
         for vertex in self.graph:
@@ -29,19 +29,19 @@ class Graph:
                 queue = [vertex]
 
                 while queue:
-                    node = queue.pop(0)
+                    node = queue.pop(0) 
                     if node not in traversal:
                         traversal.append(node)
+                        
+                        for edge in self.graph.get(node,[]):
+                            if edge not in traversal:
+                                queue.append(edge)
 
-                        for i in self.graph.get(node,[]):
-                            if i not in traversal:
-                                queue.append(i)
-        return traversal
+        return traversal    
 
-    def Dfs(self):
+    def Dfs (self):
         traversal = []
 
-     
         def Dfs_util(vertex):
             traversal.append(vertex)
 
@@ -50,15 +50,13 @@ class Graph:
                     Dfs_util(edge)
 
         for vertex in self.graph:
-            if vertex not in traversal:
-                Dfs_util(vertex)
-                        
+            if vertex  not in traversal:
+                Dfs_util(vertex)    
 
-        return traversal            
+        return traversal               
 
-
-
-        
+                                 
+       
 
 graph = Graph()
 
@@ -70,6 +68,3 @@ graph.add_edges(3,4)
 
 print(graph.Bfs())
 print(graph.Dfs())
-
-
-        
--- Trie/trie_practice.py
+++ Trie/trie_practice.py
@@ -6,14 +6,16 @@ class TrieNode:
 class Trie:
     def __init__(self) -> None:
         self.root = TrieNode()
-    
+
     def insert(self,word):
         node = self.root
 
         for char in word:
             if char not in node.child:
                 node.child[char] = TrieNode()
-            node = node.child[char]
+
+            node = node.child[char]  
+
         node.end = True
 
     def search(self,word):
@@ -21,32 +23,32 @@ class Trie:
         for char in word:
             if char not in node.child:
                 return False
-            node = node.child[char]
-        return node.end 
-
+            node = node.child[char] 
+        return node.end        
     
     def prefix_search(self,prefix):
         node = self.root
         new = []
+
         for char in prefix:
             if char not in node.child:
                 return []
             node = node.child[char]
 
-        self.prefix_word_searcher(node,prefix,new)  
+        self.prefix_searcher(node,prefix,new)
+
         return new
 
-    def prefix_word_searcher(self,node,prefix,new):
+
+    def prefix_searcher(self,node,prefix,new):
         if node.end:
-            new.append(prefix)   
+            new.append(prefix)
 
-        for char , child_node in node.child.items():
-            self.prefix_word_searcher(child_node,prefix+char,new)
-      
+        for char ,child_node in node.child.items():
+            self.prefix_searcher(child_node,prefix+char,new)  
 
- 
-        
-    
+
+   
 trie = Trie()
 
 name = ['messi','romero','depaul','ronaldo']   
@@ -56,4 +58,3 @@ for i in name:
 print(trie.prefix_search('') )
 
 
-          
